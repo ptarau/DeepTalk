3b4e3ed501a9c7f61e33d6148275bfae5abdf4b5
Paul Tarau <ptarau@gmail.com>
2019-12-20 09:52:00 -0600

    clean-up before setup

--- setup.py
+++ setup.py
@@ -3,32 +3,12 @@ from sys import platform
 import os
 from setuptools import setup
 
-# if __name__ == '__main__':
-#
-
-#     RELEASE_VERSION = True
-#
-#     if not RELEASE_VERSION:
-#         # Append incremental version number from git
-#         try:
-#             version += (
-#                 ".dev"
-#                 + subprocess.check_output(["git", "rev-list", "--count", "HEAD"])
-#                 .decode()
-#                 .strip()
-#             )
-#             print(f'version = {version}')
-#         except (subprocess.CalledProcessError, FileNotFoundError):
-#             # The .git directory has been removed (likely by setup.py sdist),
-#             # and/or git is not installed
-#             version += ".dev0"
-#             print(f'version = {version}')
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 with open("README.md","r") as f:
     long_description = f.read()
 
-version = "0.0.4"
+version = "0.0.5"
 setup(name='deep_talk',
   version=version,
   description='DeepTalk: Python and Prolog based dialogue agent',
